5f65a9324d804ca096a4e95975821ee554eaf093
Soon Sam Ribeiro Santos <karatesoon@gmail.com>
2018-07-11 17:54:56 -0300

    Implementing the player

--- app/src/main/java/com/soon/karat/exoplayer/VideoPlayerActivity.java
+++ app/src/main/java/com/soon/karat/exoplayer/VideoPlayerActivity.java
@@ -1,18 +1,13 @@
 package com.soon.karat.exoplayer;
 
-import android.Manifest;
-import android.app.Activity;
+import android.arch.lifecycle.ViewModel;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.Nullable;
-import android.support.design.widget.Snackbar;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.text.Html;
-import android.view.View;
+import android.util.Log;
 
 import com.google.android.exoplayer2.ExoPlayerFactory;
 import com.google.android.exoplayer2.SimpleExoPlayer;
@@ -30,37 +25,88 @@ import com.google.android.exoplayer2.util.Util;
 
 public class VideoPlayerActivity extends AppCompatActivity {
 
+    private static final String TAG = "VideoPlayerActivity";
+    
     private static final String APPLICATION_NAME = "Exoplayer";
+
+    private Uri videoUri;
+    private SimpleExoPlayer player;
+    private DefaultTrackSelector trackSelector;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_player);
+        Log.i(TAG, "onCreate: CREATING");
 
         // 1. Get the video URI from MainActivity.
         Intent intent = getIntent();
-        Uri videoUri = Uri.parse(intent.getStringExtra("videoUri"));
-
-        // 2. Load the uri into the PlayerView.
-        loadPlayerView(videoUri);
+        videoUri = Uri.parse(intent.getStringExtra("videoUri"));
 
 
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart: STARTING");
+        if (Util.SDK_INT > 23) {
+            Log.i(TAG, "onStart: SDK_INT: " + Util.SDK_INT + " initializing in onStart");
+            initializePlayer(videoUri);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume: RESUMING");
+        // Version 23 or lower OR player have not being initialized yet.
+        if (Util.SDK_INT <= 23 || player == null) {
+            Log.i(TAG, "onResume: SDK_INT: " + Util.SDK_INT + " initializing in onResume");
+            initializePlayer(videoUri);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause: PAUSING");
+        if (Util.SDK_INT <= 23) {
+            Log.i(TAG, "onPause: SDK_INT: " + Util.SDK_INT + " releasing in onPause");
+            releasePlayer();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop: STOPPING");
+        if (Util.SDK_INT > 23) {
+            Log.i(TAG, "onStop: SDK_INT: " + Util.SDK_INT + " releasing in onStop");
+            releasePlayer();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onDestroy: DESTROYING");
     }
 
     /**
      * Loads a video uri into a PlayerView usign MediaSource and SimpleExoPlayer.
      * @param videoUri the uri of the video that will be loaded.
      */
-    private void loadPlayerView(Uri videoUri) {
+    private void initializePlayer(Uri videoUri) {
 
         // 1. Create a default TrackSelector
         Handler mainHandler = new Handler();
         BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
         TrackSelection.Factory videoTrackSelectionFactory = new AdaptiveTrackSelection.Factory(bandwidthMeter);
-        DefaultTrackSelector trackSelector = new DefaultTrackSelector(videoTrackSelectionFactory);
+        trackSelector = new DefaultTrackSelector(videoTrackSelectionFactory);
 
         // 2. Create the player
-        SimpleExoPlayer player = ExoPlayerFactory.newSimpleInstance(this, trackSelector);
+        player = ExoPlayerFactory.newSimpleInstance(this, trackSelector);
 
         // 3. Attach the player to a view
         PlayerView mPlayerView = findViewById(R.id.player_view);
@@ -82,5 +128,13 @@ public class VideoPlayerActivity extends AppCompatActivity {
         player.prepare(videoSource);
         player.setPlayWhenReady(true);
 
+    }
+
+    private void releasePlayer() {
+        if (player != null) {
+            player.release();
+            player = null;
+            trackSelector = null;
+        }
     }
 }
